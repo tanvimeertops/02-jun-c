488e76aea2c116bc386d12e522007e51c61203ee
tanvimeertops <109589213+tanvimeertops@users.noreply.github.com>
2026-06-11 12:26:10 +0000

    11 june 2

---
+++ InputOutput1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/*
+Output : printf
+Input : scanf 
+*/
+int main(){
+    int marks;
+    printf("Enter marks :");
+    scanf("%d",&marks);
+    printf("marks is :%d",marks);
+    return 0;
+}
---
+++ InputOutput2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    float price;
+    printf("Enter price :");
+    scanf("%f",&price);
+    printf("Price is %f",price);
+    return 0;
+}
